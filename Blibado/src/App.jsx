5f1cc0550420978f94f20a00fa3c04a1c9c32599
Chrstjan <ps5christian@hotmail.com>
2024-08-22 12:18:35 +0200

    Made the gallery

--- Blibado/src/App.jsx
+++ Blibado/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <Main>
         <About videoSrc={"./src/assets/pexels-taryn-elliott-9583751 (720p).mp4"}/>
         <Locations />
-        <Gallery galleryThumbnail={"./src/assets/images/Location-06-04.jpg"}/>
+        <Gallery />
       </Main>
       <Footer>
         <LocationFrame locationUrl={"https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3069.12259614415!2d9.966123686014548!3d57.04816051297148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sda!2sdk!4v1724229628562!5m2!1sda!2sdk"}/>
--- Blibado/src/components/Main/Gallery/Gallery.jsx
+++ Blibado/src/components/Main/Gallery/Gallery.jsx
@@ -1,17 +1,25 @@
+import {useState} from "react"
 import style from "./Gallery.module.scss"
 
-export const Gallery = ({galleryThumbnail}) => {
+export const Gallery = () => {
+    const [thumbnail, setThumbnail] = useState("./src/assets/images/Location-06-04.jpg")
+
+    const handleThumbnail = (e) => {
+        let src = e.target.src;
+        const imageSrc = src.split("/localhost:5174/")[1];
+        setThumbnail(imageSrc);
+    }
     return (
         <>
             <section className={style.galleryStyling}>
                 <h2>Aktuelt</h2>
                 <div className={style.imagesGallery}>
-                    <img src={galleryThumbnail} />
+                    <img src={thumbnail} />
                     <figure className={style.galleryContainer}>
-                        <img src="/src/assets/images/Location-06-01.jpg" />
-                        <img src="/src/assets/images/Location-06-02.jpg" />
-                        <img src="/src/assets/images/Location-06-03.jpg" />
-                        <img src="/src/assets/images/Location-06-04.jpg" />
+                        <img src="/src/assets/images/Location-06-01.jpg" onClick={handleThumbnail} />
+                        <img src="/src/assets/images/Location-06-02.jpg" onClick={handleThumbnail} />
+                        <img src="/src/assets/images/Location-06-03.jpg" onClick={handleThumbnail} />
+                        <img src="/src/assets/images/Location-06-04.jpg" onClick={handleThumbnail} />
                     </figure>
                 </div>
             </section>
