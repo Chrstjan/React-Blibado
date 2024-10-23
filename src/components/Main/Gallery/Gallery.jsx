a1a73bef87ee7e63b7e18a0b7cb26d4a7ccc5135
Chrstjan <ps5christian@hotmail.com>
2024-10-23 09:53:25 +0200

    Gallery fix

--- src/components/Main/Gallery/Gallery.jsx
+++ src/components/Main/Gallery/Gallery.jsx
@@ -2,12 +2,11 @@ import {useState} from "react"
 import style from "./Gallery.module.scss"
 
 export const Gallery = () => {
-    const [thumbnail, setThumbnail] = useState("./src/assets/images/Location-06-04.jpg")
+    const [thumbnail, setThumbnail] = useState("./Location-06-04.jpg")
 
     const handleThumbnail = (e) => {
         let src = e.target.src;
-        const imageSrc = src.split("/localhost:5174/")[1];
-        setThumbnail(imageSrc);
+        setThumbnail(src);
     }
     return (
         <>
@@ -16,10 +15,10 @@ export const Gallery = () => {
                 <div className={style.imagesGallery}>
                     <img src={thumbnail} />
                     <figure className={style.galleryContainer}>
-                        <img src="./src/assets/images/Location-06-01.jpg" onClick={handleThumbnail} />
-                        <img src="./src/assets/images/Location-06-02.jpg" onClick={handleThumbnail} />
-                        <img src="./src/assets/images/Location-06-03.jpg" onClick={handleThumbnail} />
-                        <img src="./src/assets/images/Location-06-04.jpg" onClick={handleThumbnail} />
+                        <img src="./Location-06-01.jpg" onClick={handleThumbnail} />
+                        <img src="./Location-06-02.jpg" onClick={handleThumbnail} />
+                        <img src="./Location-06-03.jpg" onClick={handleThumbnail} />
+                        <img src="./Location-06-04.jpg" onClick={handleThumbnail} />
                     </figure>
                 </div>
             </section>
