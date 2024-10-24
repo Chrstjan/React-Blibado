9dd188ee384b495273a8701fb4dc442f0da0bba4
Chrstjan <ps5christian@hotmail.com>
2024-10-24 13:33:22 +0200

    Fixed the cookie banner

--- src/components/CookieBanner/CookieBanner.jsx
+++ src/components/CookieBanner/CookieBanner.jsx
@@ -3,9 +3,8 @@ import ReactGA from "react-ga4";
 import style from "./CookieBanner.module.scss"
 
 export const CookieBanner = () => {
-
     const [withAnalytics, setWithAnalytics] = useState(false);
-    const [hasUserCookie, setHasUserCookie] = useState();
+    const [hasUserCookie, setHasUserCookie] = useState(null);
 
     if (withAnalytics === true) {
       //Initialize google analytics with GTag ID
@@ -14,30 +13,37 @@ export const CookieBanner = () => {
 
     const enableGa = () => {
         setWithAnalytics(true);
-        localStorage.setItem('userAccept', true);
+        localStorage.setItem('userAccept', 'true');
+        setHasUserCookie('true');
     }
 
     const disableGa = () => {
         setWithAnalytics(false);
-        localStorage.setItem('userAccept', false);
+        localStorage.setItem('userAccept', 'false');
+        setHasUserCookie('false'); // Update the user cookie state
     }
 
-
     useEffect(() => {
-        const hasUserAccepted =  localStorage.getItem("userAccept");
-        setHasUserCookie(hasUserAccepted)
-    }, [withAnalytics])
+        const hasUserAccepted = localStorage.getItem("userAccept");
+        setHasUserCookie(hasUserAccepted);
+        if (hasUserAccepted === 'true') {
+            setWithAnalytics(true); // Enable GA if the user accepted cookies previously
+        }
+    }, []);
 
-
-  return (
-    <>{!hasUserCookie ? <div className={style.cookieBanner}>
-    <section>
-        <p>This site uses cookies for tracking purposes</p>
-    </section>
-    <section>
-        <button onClick={() => disableGa()}>Deny</button>
-        <button onClick={() => enableGa()}>Accept</button>
-    </section>
-</div> : null}</>
-  )
+    return (
+        <>
+            {!hasUserCookie && (
+                <div className={style.cookieBanner}>
+                    <section>
+                        <p>This site uses cookies for tracking purposes</p>
+                    </section>
+                    <section>
+                        <button onClick={disableGa}>Deny</button>
+                        <button onClick={enableGa}>Accept</button>
+                    </section>
+                </div>
+            )}
+        </>
+    );
 }
